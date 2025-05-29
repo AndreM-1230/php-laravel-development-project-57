0a01ca6a845a33db69db640deefc50129e013b61
Andre <ymottnaf@yandex.ru>
2025-05-29 21:53:33 +0300

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,8 +44,9 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">Уже зарегистрированы?</a>
-
-            <x-primary-button class="ms-4">Зарегистрировать</x-primary-button>
+            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+                Зарегистрировать
+            </button>
         </div>
     </form>
 @endsection
